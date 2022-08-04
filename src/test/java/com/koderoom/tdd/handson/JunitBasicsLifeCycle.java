3f88d7e03b0ed5f49091858987d2dd34719b48cc
santosh mondal <santosh.ece06@gmail.com>
2022-08-04 12:59:00 +0530

    Test Suites.

--- src/test/java/com/koderoom/tdd/handson/JunitBasicsLifeCycle.java
+++ src/test/java/com/koderoom/tdd/handson/JunitBasicsLifeCycle.java
@@ -1,7 +1,13 @@
 package com.koderoom.tdd.handson;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 @Slf4j
 @DisplayName("JunitBasicsLifeCycle")
--- src/test/java/com/koderoom/tdd/suites/AllUnitTestsV1.java
+++ src/test/java/com/koderoom/tdd/suites/AllUnitTestsV1.java
@@ -1,10 +1,13 @@
 package com.koderoom.tdd.suites;
 
 
-import org.junit.platform.suite.api.*;
+import org.junit.platform.suite.api.IncludePackages;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
 
+@SelectPackages("com.koderoom.tdd.handson")
 @Suite
 @SuiteDisplayName("AllUnitTestsV1")
-@IncludePackages({"com.koderoom.tdd.handson"})
 public class AllUnitTestsV1 {
 }
