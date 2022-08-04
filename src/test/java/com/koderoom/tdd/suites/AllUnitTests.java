9760c8478030cbb33ab461979834780bad48b319
santosh mondal <santosh.ece06@gmail.com>
2022-08-04 11:53:39 +0530

    Test Suites.

--- src/test/java/com/koderoom/tdd/suites/AllUnitTests.java
+++ src/test/java/com/koderoom/tdd/suites/AllUnitTests.java
@@ -7,8 +7,10 @@ import com.koderoom.tdd.handson.JunitBasicsHandsonV2;
 import com.koderoom.tdd.handson.JunitBasicsLifeCycle;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
+@SuiteDisplayName("All Unit Test using Classes")
 @SelectClasses({JunitBasicsHandson.class, JunitBasicsHandsonV1.class, JunitBasicsHandsonV2.class, JunitBasicsLifeCycle.class})
 public class AllUnitTests {
 }
---
+++ src/test/java/com/koderoom/tdd/suites/AllUnitTestsV1.java
@@ -0,0 +1,10 @@
+package com.koderoom.tdd.suites;
+
+
+import org.junit.platform.suite.api.*;
+
+@Suite
+@SuiteDisplayName("AllUnitTestsV1")
+@IncludePackages({"com.koderoom.tdd.handson"})
+public class AllUnitTestsV1 {
+}
