f85fabfed577901380292cdc57ffbdd3d7f1de2a
santosh mondal <santosh.ece06@gmail.com>
2022-08-04 13:06:21 +0530

    Test Suites.

--- src/test/java/com/koderoom/tdd/suites/AllUnitTestsV1.java
+++ src/test/java/com/koderoom/tdd/suites/AllUnitTestsV1.java
@@ -1,13 +1,14 @@
 package com.koderoom.tdd.suites;
 
 
+import org.junit.platform.suite.api.ExcludePackages;
 import org.junit.platform.suite.api.IncludePackages;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 
-@SelectPackages("com.koderoom.tdd.handson")
 @Suite
 @SuiteDisplayName("AllUnitTestsV1")
+@SelectPackages("com.koderoom.tdd")
 public class AllUnitTestsV1 {
 }
