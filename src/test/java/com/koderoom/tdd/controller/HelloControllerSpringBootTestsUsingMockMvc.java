e18927e5c91bc57e4b626632766b90f9897f6a07
santosh mondal <santosh.ece06@gmail.com>
2022-08-04 22:14:03 +0530

    Spring Boot Test using Full Contxt and TestRestTemplate.

---
+++ src/test/java/com/koderoom/tdd/controller/HelloControllerSpringBootTestsUsingMockMvc.java
@@ -0,0 +1,61 @@
+package com.koderoom.tdd.controller;
+
+import com.koderoom.tdd.model.Employee;
+import org.assertj.core.api.Assertions;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerSpringBootTestsUsingMockMvc {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setup() {
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    void test1() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello/"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("Hello World!")))
+        ;
+    }
+
+    @Test
+    void test2() throws Exception{
+        Employee employee = new Employee(1, "raj");
+        mockMvc.perform(MockMvcRequestBuilders
+                    .post("/hello/v1")
+                    .content(objectMapper.writeValueAsString(employee))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("raj")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.is("raj")))
+        ;
+    }
+
+}
