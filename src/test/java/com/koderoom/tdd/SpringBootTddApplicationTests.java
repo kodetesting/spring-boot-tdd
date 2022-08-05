e5ba3a3d57db81fba0871acc9fae550e18784c58
santosh mondal <santosh.ece06@gmail.com>
2022-08-05 14:09:11 +0530

    Config Property in Main.

--- src/test/java/com/koderoom/tdd/SpringBootTddApplicationTests.java
+++ src/test/java/com/koderoom/tdd/SpringBootTddApplicationTests.java
@@ -1,13 +1,28 @@
 package com.koderoom.tdd;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@PropertySource(value = "file:${config-file-path}")
 class SpringBootTddApplicationTests {
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	Environment env;
 
 	@Test
 	void contextLoads() {
+		String value = env.getProperty("server.port");
+		log.info("Value {}", value);
 	}
 
 }
