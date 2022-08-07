0aae5fd6e11333d86b6a6f7d8356ad9680385ee3
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-08-07 15:04:37 +0530

    github action

--- src/test/java/com/koderoom/tdd/SpringBootTddApplicationTests.java
+++ src/test/java/com/koderoom/tdd/SpringBootTddApplicationTests.java
@@ -1,9 +1,11 @@
 package com.koderoom.tdd;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@AutoConfigureTestDatabase
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootTddApplicationTests {
 
 	@Test
