f7d558fede8fb535d4c7c7580e80394eb9624a47
santosh mondal <santosh.ece06@gmail.com>
2022-08-22 19:34:07 +0530

    spring-data-mongo tdd setup conintue..

---
+++ src/test/java/com/koderoom/tdd/controller/UserControllerSpringBootTests.java
@@ -0,0 +1,52 @@
+package com.koderoom.tdd.controller;
+
+import com.koderoom.tdd.model.Employee;
+import com.koderoom.tdd.model.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@AutoConfigureDataMongo
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserControllerSpringBootTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+    private String baseUrl;
+
+    @BeforeEach
+    void setUpBeforeEach() {
+        baseUrl = "http://localhost:" + port;
+    }
+
+    @Test
+    void loadContext(){}
+
+    @Test
+    void test1() {
+        String url = baseUrl + "/user/hi";
+        String actual = this.restTemplate.getForObject(url, String.class);
+        Assertions.assertThat(actual).isNotNull().isEqualTo("Hello World!");
+    }
+
+    @Test
+    void test2() {
+        String url = baseUrl + "/user/?entityUid=test";
+        ResponseEntity responseEntity = this.restTemplate.getForEntity(url, List.class);
+
+        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+}
--- src/test/java/com/koderoom/tdd/controller/UserControllerTest.java
+++ src/test/java/com/koderoom/tdd/controller/UserControllerTest.java
@@ -1,19 +1,15 @@
 package com.koderoom.tdd.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.koderoom.tdd.model.Employee;
 import com.koderoom.tdd.model.User;
 import com.koderoom.tdd.service.UserServiceImpl;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
