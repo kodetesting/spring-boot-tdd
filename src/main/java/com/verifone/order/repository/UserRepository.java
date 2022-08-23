cd6cf0baf07672fb89b38cdcbb99f8699ffdb282
santosh mondal <santosh.ece06@gmail.com>
2022-08-23 08:57:11 +0530

    clean up of user model.

--- src/main/java/com/verifone/order/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.verifone.order.repository;
-
-import com.verifone.order.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends MongoRepository<User, String> {
-}
