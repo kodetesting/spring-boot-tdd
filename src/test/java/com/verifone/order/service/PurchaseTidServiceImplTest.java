af8977c5a89636fa004aec85272b7c51e5751227
santosh mondal <santosh.ece06@gmail.com>
2022-08-23 14:24:54 +0530

    WIP Test Cases are in progress. For Service Layer.

--- src/test/java/com/verifone/order/service/PurchaseTidServiceImplTest.java
+++ src/test/java/com/verifone/order/service/PurchaseTidServiceImplTest.java
@@ -16,8 +16,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @ExtendWith(MockitoExtension.class)
 class PurchaseTidServiceImplTest {
 
